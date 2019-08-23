e24ce99e4a96e378695e8ba4b0df9f3850e01f21
Leonardo Liao <leonardo.q.liao@student.uts.edu.au>
2019-08-23 14:55:52 +1000

    Enable routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+// import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    // CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
